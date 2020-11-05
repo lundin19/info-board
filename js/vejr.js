7702c9fa5b38b3bccf8f6bc1809d6e04c751f4d6
Markus  backhausen <markusbackhausen@outlook.com>
2020-11-05 10:26:16 +0100

    mbbbb

--- js/vejr.js
+++ js/vejr.js
@@ -1,29 +1,28 @@
-// PROXY => CORS Anywhere helps with accessing data from other websites that is normally forbidden by the same origin policy of web browsers. This is done by proxying requests to these sites via a server (written in Node.js, in this case).
-const proxy = 'https://cors-anywhere.herokuapp.com/';// API's
 
+// PROXY => CORS Anywhere helps with accessing data from other websites that is normally forbidden by the same origin policy of web browsers. This is done by proxying requests to these sites via a server (written in Node.js, in this case).
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+// API's
 // 1§ vejr eu api
 let vejrEu = `${proxy}https://vejr.eu/api.php?location=Aalborg&degree=C%27))`; //henter API ned
-
 // §2 DarkSky api
 let darkSky = `${proxy}https://api.darksky.net/forecast/2f35c97a3f5b8edad9aecb3fa76058cb/57.0488,9.9217`;
 console.log("fetching data");
 
-/// 1§ VEJR EU API
 
+/// 1§ VEJR EU API
 // GET => temperature & skyText
 fetch(vejrEu) 
-
 .then((response) => {
     return response.json(); 
 })
-
 .then((data) => {
     // Send to View section
     buildView(data)
 })
-
 .catch(
 );
+
 
 /// 2§ DARK SKY API
 // GET => Icon
@@ -31,45 +30,47 @@ fetch(darkSky)
 .then((response) => {
     return response.json(); 
 })
-
 .then((data) => {
     // Send to View section
     buildIcon(data) 
 })
-
 .catch(
 );
 
+
+
 /// View ///
+
 function buildView(data) {
     // Declare data 
     const {temperature, skyText} = data.CurrentData;
-
     // Create current date
     let today = new Date();
     let dd = String(today.getDate()).padStart(2, '0');
     let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     let yyyy = today.getFullYear();
-
     // Date Structure
     today = `${dd}-${mm}-${yyyy}`;
-
     // Set value
-    document.querySelector('.time').innerHTML = today
-    document.querySelector('.temp').innerHTML = temperature + "&#176"
-    document.querySelector('.summary').innerHTML = skyText
+    document.querySelector('.time').innerHTML = today;
+    document.querySelector('.date').innerHTML = date;
+    document.querySelector('.temp').innerHTML = temperature + '&#176';
+    document.querySelector('.summary').innerHTML = skyText;
+    
+    
+    
+    
 }
-
 /// Icon
 function buildIcon(data) {
-    const {icon} = data.currently;   
-
+    const {icon} = data.currently;                  
     // Call getIcon
-    document.querySelector('.icon').src = '/img/svg/' + getIcon(icon)
+    document.querySelector('.icon').src = 'img/svg/' + getIcon(icon) 
 }
-
 // Check current date and return appropriate src string
-function getIcon(icon) {    switch (icon) {
+function getIcon(icon) {
+   
+    switch (icon) {
         // If status === case => then return matching weather icon 
         case 'clear-day': return "wi-day-sunny.svg";
         case 'clear-night': return "wi-night-clear.svg";
@@ -82,20 +83,24 @@ function getIcon(icon) {    switch (icon) {
         case 'sleet': return "wi-sleet.svg.svg";
         case 'snow': return "wi-snow.svg";
         case 'wind': return "wi-windy.svg";
-        case 'fog': return "wi-fog.svg";        
-        default: return "";       
-    }    
-    
-    /* DarkSky weather types:        
-    clear-day
-    clear-night
-    partly-cloudy-day
-    partly-cloudy-night
-    cloudy
-    rain
-    sleet
-    snow
-    wind
-    fog */
-}
+        case 'fog': return "wi-fog.svg";
 
+        default: return "";       
+    }
+
+    
+    /* DarkSky weather types:
+
+        clear-day
+        clear-night
+        partly-cloudy-day
+        partly-cloudy-night
+        cloudy
+        rain
+        sleet
+        snow
+        wind
+        fog
+    
+    */
+}
